6545accf9811ce753a6ae7e488539afa71fb1705
Toberumono <JoshuaLipstone@gmail.com>
2015-08-04 10:44:52 -0400

    Added a path modified time sorting method.

--- src/toberumono/structures/SortingMethods.java
+++ src/toberumono/structures/SortingMethods.java
@@ -1,58 +1,105 @@
 package toberumono.structures;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Comparator;
 
+import toberumono.structures.collections.lists.SortedList;
+
 /**
- * Some pre-configured {@link java.util.Comparator Comparators} for common sorting methods
+ * A static class containing some common {@link Comparator Comparators} for sorting methods.
  * 
  * @author Toberumono
+ * @see SortedList
  */
 public class SortingMethods {
 	
 	/**
-	 * A Comparator that sorts {@link java.lang.String Strings} in ascending alphabetical order.
+	 * A {@link Comparator} that sorts {@link String Strings} in ascending alphabetical order.
+	 * 
+	 * @see #STRING_ALPHABETICAL_DESCENDING
 	 */
-	public static final Comparator<String> STRING_ALPHABETICAL_ASCENDING = (o1, o2) -> {
-		return o1.compareTo(o2);
-	};
-	/**
-	 * A Comparator that sorts {@link java.lang.String Strings} in descending alphabetical order.
-	 */
-	public static final Comparator<String> STRING_ALPHABETICAL_DESCENDING = (o2, o1) -> {
-		return o1.compareTo(o2);
-	};
+	public static final Comparator<String> STRING_ALPHABETICAL_ASCENDING = (o1, o2) -> o1.compareTo(o2);
 	
 	/**
-	 * A Comparator that sorts {@link java.lang.String Strings} in ascending length order, with ascending lexographical order
+	 * A {@link Comparator} that sorts {@link String Strings} in descending alphabetical order.
+	 * 
+	 * @see #STRING_ALPHABETICAL_ASCENDING
+	 */
+	public static final Comparator<String> STRING_ALPHABETICAL_DESCENDING = (o1, o2) -> o2.compareTo(o1); //o2.compareTo(o1) is the reverse of o1.compareTo(o2), thereby making this descending.
+	
+	/**
+	 * A {@link Comparator} that sorts {@link String Strings} in ascending length order, with ascending lexographical order
 	 * as a secondary sorting mechanism for strings of the same length.
+	 * 
+	 * @see #STRING_LENGTH_DESCENDING
 	 */
-	public static final Comparator<String> STRING_LENGTH_ASCENDING = (o1, o2) -> {
-		return o1.length() > o2.length() ? 1 : (o1.length() == o2.length() ? o1.compareTo(o2) : -1);
-	};
-	/**
-	 * A Comparator that sorts {@link java.lang.String Strings} in descending length order, with descending lexographical
-	 * order as a secondary sorting mechanism for strings of the same length.
-	 */
-	public static final Comparator<String> STRING_LENGTH_DESCENDING = (o1, o2) -> {
-		return o2.length() > o1.length() ? 1 : (o1.length() == o2.length() ? o2.compareTo(o1) : -1);
-	};
+	public static final Comparator<String> STRING_LENGTH_ASCENDING = (o1, o2) -> o1.length() < o2.length() ? -1 : (o1.length() == o2.length() ? o1.compareTo(o2) : 1);
 	
 	/**
-	 * A Comparator that sorts numbers in ascending order, taking into account that the long to double conversion can lose
-	 * precision.
+	 * A {@link Comparator} that sorts {@link String Strings} in descending length order, with descending lexographical order
+	 * as a secondary sorting mechanism for strings of the same length.
+	 * 
+	 * @see #STRING_LENGTH_ASCENDING
+	 */
+	public static final Comparator<String> STRING_LENGTH_DESCENDING = (o1, o2) -> o2.length() < o1.length() ? -1 : (o2.length() == o1.length() ? o2.compareTo(o1) : 1);
+	
+	/**
+	 * A {@link Comparator} that sorts numbers in ascending order, taking into account that the {@link Long} to
+	 * {@link Double} conversion can lose precision.
+	 * 
+	 * @see #NUMERIC_DESCENDING
 	 */
 	public static final Comparator<Number> NUMERIC_ASCENDING = (o1, o2) -> {
 		if (o1 instanceof Long && o2 instanceof Long)
 			return ((Long) o1).compareTo((Long) o2);
 		return o1.doubleValue() > o2.doubleValue() ? 1 : (o1.doubleValue() == o2.doubleValue() ? 0 : -1);
 	};
+	
 	/**
-	 * A Comparator that sorts numbers in descending order, taking into account that the long to double conversion can lose
-	 * precision.
+	 * A {@link Comparator} that sorts numbers in descending order, taking into account that the {@link Long} to
+	 * {@link Double} conversion can lose precision.
+	 * 
+	 * @see #NUMERIC_ASCENDING
 	 */
 	public static final Comparator<Number> NUMERIC_DESCENDING = (o1, o2) -> {
 		if (o1 instanceof Long && o2 instanceof Long)
 			return ((Long) o2).compareTo((Long) o1);
 		return o2.doubleValue() > o1.doubleValue() ? 1 : (o1.doubleValue() == o2.doubleValue() ? 0 : -1);
 	};
+	
+	/**
+	 * A {@link Comparator} that sorts {@link Path Paths} by date modified in ascending order.<br>
+	 * In the event of an {@link IOException} from {@link Files#getLastModifiedTime(Path, java.nio.file.LinkOption...)}, the
+	 * {@link Comparator} returns {@code 0}.
+	 * 
+	 * @see #PATH_MODIFIED_TIME_DESCENDING
+	 */
+	public static final Comparator<Path> PATH_MODIFIED_TIME_ASCENDING = (o1, o2) -> {
+		try {
+			return Files.getLastModifiedTime(o1).compareTo(Files.getLastModifiedTime(o2)); //Sort the Paths by last modified.  The first element will always be the oldest this way.
+		}
+		catch (IOException e) {
+			return 0;
+		}
+	};
+	
+	/**
+	 * A {@link Comparator} that sorts {@link Path Paths} by date modified in ascending order.<br>
+	 * In the event of an {@link IOException} from {@link Files#getLastModifiedTime(Path, java.nio.file.LinkOption...)}, the
+	 * {@link Comparator} returns {@code 0}.
+	 * 
+	 * @see #PATH_MODIFIED_TIME_ASCENDING
+	 */
+	public static final Comparator<Path> PATH_MODIFIED_TIME_DESCENDING = (o1, o2) -> {
+		try {
+			return Files.getLastModifiedTime(o2).compareTo(Files.getLastModifiedTime(o1)); //Sort the Paths by last modified.  The first element will always be the oldest this way.
+		}
+		catch (IOException e) {
+			return 0;
+		}
+	};
+	
+	private SortingMethods() { /*This is a static class*/ }
 }
